e43ec62596795644f10022c99ea91924a5cec4fe
Haoyu Yang <opop369a@icloud.com>
2015-04-08 21:38:09 +0800

    登录暂时完成，但是没有涉及到登录的时间控制

--- app/js/controllers/userControllers.js
+++ app/js/controllers/userControllers.js
@@ -1,43 +1,73 @@
-var userModule = angular.module('userModule',[]);
+var userModule = angular.module('userModule', ['ui.bootstrap']);
 
-userModule.controller('userLoginCtrl', ['$scope','$http','$location',
-	function($scope,$http,$location)
-	{
-        $scope.errorMessage='';
-		$scope.Login=function()
-		{
-			
-			$http({
+userModule.controller('userLoginCtrl', ['$scope','$rootScope','$log', '$modal', '$http', '$location',
+    function ($scope,$rootScope,$log, $modal, $http, $location) {
 
-                method: 'POST', 
-                url: 'http://127.0.0.1:5000/api/v1/users/login',
-                headers: {'Content-Type': 'application/json'},
-                data: { 
-                        'username': $scope.un, 
-                        'password': $scope.pw 
-                     }
-            }).
+        $scope.open = function (size) {
 
-            success(function(data, status) {
-
-                $scope.data = data;
-                if(data == 'FALSE'){
-                    $scope.errorMessage = '验证失败';
-                } else {
-                   //$scope.$apply(function() { $location.path("/index"); });
-                    $location.path("/index");
-
-                }
-
-            }).
-
-            error(function(data, status) {
-                $scope.data = data || "FALSE";
-                $scope.errorMessage = 'Something went wrong';
-                $location.path("/login");
-
+            var modalInstance = $modal.open({
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalInstanceCtrl',
+                size: size,
             });
-		}
-	}
 
-	])
+            modalInstance.result.then(function (loginInfo) {
+                $scope.username = loginInfo.un;
+                $scope.password = loginInfo.pw;
+
+                $http({
+
+                    method: 'POST',
+                    url: 'http://127.0.0.1:5000/api/v1/users/login',
+                    headers: {'Content-Type': 'application/json'},
+                    data: {
+                        "username": $scope.username,
+                        "password": $scope.password
+                    }
+                }).
+
+                    success(function (data, status) {
+
+                        $scope.data = data;
+                        if (data == 'FALSE') {
+                            $scope.errorMessage = '验证失败';
+                        } else {
+                            //$scope.$apply(function() { $location.path("/index"); });
+                            $scope.errorMessage = '登录成功'
+                            $rootScope.username = $scope.username;
+                            $location.path("/index");
+
+                        }
+
+                    }).
+
+                    error(function (data, status) {
+                        $scope.data = data || "FALSE";
+                        $scope.errorMessage = 'Something went wrong';
+                        $location.path("/login");
+
+                    });
+
+
+                //http
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+
+    }])
+
+userModule.controller('ModalInstanceCtrl', ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+
+    $scope.loginInfo = {};
+
+    $scope.ok = function () {
+        
+        $modalInstance.close($scope.loginInfo);
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+}]);
