313ebe3b121ac5e16377ef2e10e5e925a47fbbc0
justsavor <xyc9121993@gmail.com>
2015-04-09 12:10:47 +0800

    添加report资源的相关界面和功能

---
+++ app/js/controllers/reportControllers.js
@@ -0,0 +1,96 @@
+/**
+ * Created by justsavor on 15/4/9.
+ */
+var reportModule = angular.module('reportModule',[]);
+
+reportModule.factory('reportService',['$resource',function($resource){
+    return $resource('http://127.0.0.1:5000/api/v1/reports/:reportId', {reportId: '@id'}, {
+        update: {
+            method: 'PUT'
+        }
+    });
+}]);
+
+reportModule.controller('reportListCtrl',['$scope','reportService',function($scope,reportService) {
+    reportService.query(function (data) {
+        $scope.reportList = data;
+    });
+
+    $scope.gridOptions = {
+        data: 'reportList',
+        enableColumnResizing: true,
+        paginationPageSizes: [20, 50, 100],
+        paginationPageSize: 20,
+        columnDefs: [{
+            field: "title",
+            displayName: "标题",
+            width: 300,
+            cellTemplate: '<div><a ui-sref="viewReport({id:row.entity.id})">{{grid.getCellValue(row, col)}}</a></div>'
+        }, {
+            field: "reporter",
+            displayName: "报告人"
+        }, {
+            field: "reporter_title",
+            displayName: "报告人职位"
+        }, {
+            field: "company",
+            displayName: "组织"
+        }]
+    };
+}]);
+
+reportModule.controller('reportAddCtrl', ['$scope', '$state', 'reportService', 'Time', function ($scope, $state, reportService, Time) {
+    $scope.isEdit = true;
+
+    $scope.report = {};
+
+    $scope.submit = function () {
+        $scope.report.creator_id = 1;
+        $scope.report.create_time = Time.currentTime;
+
+        reportService.save($scope.report, function (data) {
+            console.log("add successful");
+            $state.go('viewReport', {id: data.id});
+        });
+    };
+}]);
+
+reportModule.controller('reportShowCtrl',['$scope','$stateParams','reportService', 'Time', function ($scope, $stateParams, reportService, Time) {
+    $scope.isEdit = false;
+
+    var id = $stateParams.id;
+
+    reportService.get({reportId:id}, function (data) {
+        $scope.report = data;
+    });
+
+    $scope.changeState = function () {
+        $scope.isEdit = !$scope.isEdit;
+
+        if($scope.isEdit){
+            $scope.origin = angular.copy($scope.report);
+        }
+
+        if(!$scope.isEdit){
+            $scope.report = $scope.origin;
+        }
+    };
+
+    $scope.getEditLabel = function () {
+        if($scope.isEdit) {
+            return "取消";
+        }else{
+            return "编辑/上传";
+        }
+    };
+
+    $scope.submit = function () {
+        $scope.report.updater_id = 1;
+        $scope.report.update_time = Time.currentTime;
+
+        $scope.report.$update(function () {
+            console.log("update ok");
+            $scope.isEdit = !$scope.isEdit;
+        })
+    };
+}]);
