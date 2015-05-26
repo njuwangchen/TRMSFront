e0c4bfec6565943d79298f27568ff86a645887d8
njuwangchen <wangchenclark0512@gmail.com>
2015-05-26 18:40:35 +0800

    评论删除和修改

--- app/js/controllers/commentControllers.js
+++ app/js/controllers/commentControllers.js
@@ -1,7 +1,11 @@
 var commentModule = angular.module('CommentModule', []);
 
 commentModule.factory('CommentService', ['$resource', function ($resource) {
-    return $resource('http://121.40.106.155:5000/api/v1/comments');
+    return $resource('http://121.40.106.155:5000/api/v1/comments', {id: '@id'}, {
+        update: {
+            method: 'PUT'
+        }
+    });
 }]);
 
 commentModule.controller('CommentCtrl', ['$scope', '$rootScope', '$stateParams', '$http', 'Time', 'CommentService', function ($scope, $rootScope, $stateParams, $http, Time, CommentService) {
@@ -27,12 +31,12 @@ commentModule.controller('CommentCtrl', ['$scope', '$rootScope', '$stateParams',
                     var commentFieldsIds = data['commentFieldsIds']
                     commentFieldsIds.forEach(function (element) {
                         $scope.commentFields.forEach(function (inner) {
-                            if(inner.id == element)
+                            if (inner.id == element)
                                 $scope.attrsShown.push(inner);
                         })
                     })
                 });
-        })
+        });
 
     var updateCommentList = function () {
         $http.post('http://121.40.106.155:5000/api/v1/comments/query', $scope.commentQuery).
@@ -42,6 +46,51 @@ commentModule.controller('CommentCtrl', ['$scope', '$rootScope', '$stateParams',
     };
 
     updateCommentList();
+
+    $scope.delete = function (comment_id) {
+        CommentService.delete({
+            id: comment_id
+        }, function (data) {
+            updateCommentList();
+        });
+    };
+
+    $scope.edit = function (comment) {
+        comment.isEdit = true;
+
+        comment.attrs = comment.content.split('&');
+    };
+
+    $scope.cancelEdit = function (comment) {
+        comment.isEdit = false;
+    };
+
+    $scope.simpleEdit = function (comment) {
+        console.log(comment);
+        comment.isEdit = false;
+        CommentService.update(comment, function (data) {
+            console.log("update success!");
+        });
+    };
+
+    $scope.detailEdit = function (comment) {
+        var result = '';
+        for (var i = 0; i < comment.attrs.length; i++) {
+            if (comment.attrs[i]) {
+                if (i == comment.attrs.length - 1)
+                    result += comment.attrs[i];
+                else
+                    result += comment.attrs[i] + "&"
+            }
+        }
+        console.log(result);
+        comment.content = result;
+        console.log(comment);
+        comment.isEdit = false;
+        CommentService.update(comment, function (data) {
+            console.log("update success!");
+        });
+    };
 
     $scope.simpleSubmit = function () {
         $scope.comment.comment_time = Time.currentTime(new Date());
@@ -57,15 +106,13 @@ commentModule.controller('CommentCtrl', ['$scope', '$rootScope', '$stateParams',
 
     $scope.detailSubmit = function () {
         $scope.comment.comment_time = Time.currentTime(new Date());
-        var result ='';
-        for(var i =0;i<$scope.attrmodels.length;i++)
-        {
-            if($scope.attrmodels[i])
-            {
-                if(i == $scope.attrmodels.length-1)
-                result+=$scope.attrmodels[i];
-            else
-                result+=$scope.attrmodels[i]+"&"
+        var result = '';
+        for (var i = 0; i < $scope.attrmodels.length; i++) {
+            if ($scope.attrmodels[i]) {
+                if (i == $scope.attrmodels.length - 1)
+                    result += $scope.attrmodels[i];
+                else
+                    result += $scope.attrmodels[i] + "&"
             }
         }
         console.log(result);
@@ -76,7 +123,7 @@ commentModule.controller('CommentCtrl', ['$scope', '$rootScope', '$stateParams',
             $scope.comment.star = 0;
         });
 
-        for(var i =0;i<$scope.attrmodels.length;i++)
+        for (var i = 0; i < $scope.attrmodels.length; i++)
             $scope.attrmodels[i] = "";
     };
 }]);
